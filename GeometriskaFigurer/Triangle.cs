a624d817ab5f2a96d7576ce6cbf9726f766206db
Abdelilah Achir <abdiachir@gmail.com>
2022-11-11 10:37:45 +0100

    changed formulas av area and perimeter of Triangle

--- GeometriskaFigurer/Triangle.cs
+++ GeometriskaFigurer/Triangle.cs
@@ -4,13 +4,12 @@ namespace GeometriskaFigurer
 {
     public class Triangle : Shape2D
     {
-        public override float Circumference => _p1.Length() + _p2.Length() + _p3.Length();
+        public override float Circumference => (MathF.Sqrt(MathF.Pow(_p2.X - _p1.X, 2) + MathF.Pow(_p2.Y - _p1.Y, 2))) +
+                                               (MathF.Sqrt( MathF.Pow(_p3.X - _p2.X  ,2) + MathF.Pow(_p3.Y - _p2.Y,2))) + 
+                                               (MathF.Sqrt(MathF.Pow(_p3.X - _p1.X,2) + MathF.Pow(_p3.Y - _p1.Y,2)));  // + _p2.Length() + _p3.Length();
 
         public override Vector3 Center => new Vector3((_p1.X + _p2.X + _p3.X) / 3, (_p1.Y + _p2.Y + _p3.Y) / 3, 0);
-        public override float Area => MathF.Round( MathF.Sqrt(((_p1.Length() + _p2.Length() + _p3.Length()) / 2F) * (_p1.Length() + _p2.Length() + _p3.Length()) *
-                                                                       (-_p1.Length() + _p2.Length() + _p3.Length()) *
-                                                                       (_p1.Length() - _p2.Length() + _p3.Length()) *
-                                                                       (_p1.Length() + _p2.Length() - _p3.Length())), 2);
+        public override float Area => MathF.Abs((_p1.X * (_p2.Y - _p3.Y) + _p2.X * (_p3.Y - _p1.Y) + _p3.X * (_p1.Y - _p2.Y)) / 2);
 
 
 
@@ -26,7 +25,7 @@ namespace GeometriskaFigurer
 
         public override string ToString()
         {
-            return $"       Triangle@( Center: ({Center.X}  {Center.Y})  Point 1: ({_p1.X}  {_p1.Y})  Point 2: ({_p2.X}  {_p2.Y})  Point 3: ({_p3.X}  {_p3.Y}))";
+            return $"       Triangle@( Center: ({Center.X}  {Center.Y})  Point 1: ({_p1.X}  {_p1.Y})  Point 2: ({_p2.X}  {_p2.Y})  Point 3: ({_p3.X}  {_p3.Y}) Area : {Area} perimeter : {Circumference})";
         }
     }
 }
