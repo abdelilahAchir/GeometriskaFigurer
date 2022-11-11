5835dcefa3776cf70c8f96e8d003045c29955c58
Abdelilah Achir <abdiachir@gmail.com>
2022-11-11 10:38:45 +0100

    Deleted unsecesary information

--- GeometriskaFigurer/Triangle.cs
+++ GeometriskaFigurer/Triangle.cs
@@ -25,7 +25,7 @@ namespace GeometriskaFigurer
 
         public override string ToString()
         {
-            return $"       Triangle@( Center: ({Center.X}  {Center.Y})  Point 1: ({_p1.X}  {_p1.Y})  Point 2: ({_p2.X}  {_p2.Y})  Point 3: ({_p3.X}  {_p3.Y}) Area : {Area} perimeter : {Circumference})";
+            return $"       Triangle@( Center: ({Center.X}  {Center.Y})  Point 1: ({_p1.X}  {_p1.Y})  Point 2: ({_p2.X}  {_p2.Y})  Point 3: ({_p3.X}  {_p3.Y}))";
         }
     }
 }
